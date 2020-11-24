70292cc5718e10adbc746858a5e5c909d4c0f886
silvianiiinimaki <silvia.niininmaki@hotmail.fi>
2020-11-24 12:43:04 +0200

    Change path in index.js.

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const tasks = require("./routes/tasks.js");
 app.use(cors());
 app.use(express.json());
-app.use("/tasks", tasks);
+app.use("/", tasks);
 
 const port = process.env.PORT || 8080;
 const server = app.listen(port, () => {
