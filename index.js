50b2916cb94da5f1fdf8d0f0de6cb3bd49184cc8
silvianiiinimaki <silvia.niininmaki@hotmail.fi>
2020-12-20 20:18:08 +0200

    Use public.

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const tasks = require("./routes/tasks.js");
 app.use(cors());
 app.use(express.json());
 app.use("/", tasks);
-app.use(express.static("public/build"));
+app.use(express.static("public"));
 
 const port = process.env.PORT || 8080;
 const server = app.listen(port, () => {
