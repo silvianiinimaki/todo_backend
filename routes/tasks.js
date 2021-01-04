9c20b8580e6b83ed48f6db61cf3a04bcb0a67171
silvianiiinimaki <silvia.niininmaki@hotmail.fi>
2021-01-04 22:43:55 +0200

    Remove "/" from edit-requests path.

--- routes/tasks.js
+++ routes/tasks.js
@@ -18,7 +18,7 @@ const main = async () => {
     connection.findById(userId).then((data) => res.send(data));
   });
   // Edit task
-  tasks.post(`/task/:taskId([1-9]+)/`, async (req, res) => {
+  tasks.post(`/task/:taskId([1-9]+)`, async (req, res) => {
     const taskId = Number(req.params.taskId);
     const column = req.body;
     connection.edit(column, taskId).then(() => res.send("task saved"));
